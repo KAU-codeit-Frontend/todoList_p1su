6fcc7bbd65eff7af31dfa22736ba26635615be56
P1su <pissssssu@naver.com>
2024-08-15 21:49:37 +0900

    feat: TodoList 컴포넌트 생성 후 List 컴포넌트에 배치

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,7 +1,14 @@
+import TodoList from './TodoList/TodoList';
+
 const List = () => {
   return(
     <div>
       <h5>Todo List</h5>
+      <form>
+        <input placeholder='검색어를 입력하세요'/>
+      </form>
+      
+      <TodoList />
     </div>
   );
 };
---
+++ src/components/List/TodoList/TodoList.tsx
@@ -0,0 +1,15 @@
+const TodoList = () => {
+  return(
+    <div>
+      <form>
+        <input type='checkbox'/>
+        <p>Todo...</p>
+        <p>Date</p>
+        <button type='submit'>
+          삭제
+        </button>
+      </form>
+    </div>
+  );
+};
+export default TodoList;
