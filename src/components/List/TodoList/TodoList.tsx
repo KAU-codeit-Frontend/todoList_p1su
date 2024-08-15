c5008bfbdc0ac92bf6764e2b358f49f99b722a48
P1su <pissssssu@naver.com>
2024-08-15 23:35:12 +0900

    feat: TodoList 스타일 코드 적용

--- src/components/List/TodoList/TodoList.tsx
+++ src/components/List/TodoList/TodoList.tsx
@@ -1,13 +1,17 @@
+import './TodoList.css';
+
 const TodoList = () => {
   return(
-    <div>
-      <form>
-        <input type='checkbox'/>
-        <p>Todo...</p>
-        <p>Date</p>
-        <button type='submit'>
-          삭제
-        </button>
+    <div className='todo-wrapper'>
+      <form className='todo-form'>
+        <input className='checkbox' type='checkbox'/>
+        <p className='todo'>Todo...</p>
+        <section className='right-section'>
+          <p>Date</p>
+          <button type='submit'>
+            삭제
+          </button>
+        </section>
       </form>
     </div>
   );
