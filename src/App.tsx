55759847fa202e4c028ea1f11df84471843801a0
P1su <pissssssu@naver.com>
2024-08-15 21:39:23 +0900

    feat: Editor 파일 생성 후 컴포넌트 배치

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+import Editor from './components/Editor/Editor';
 import Header from './components/Header/Header';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <div>
       <Header />
+      <Editor />
     </div>
   );
 }
