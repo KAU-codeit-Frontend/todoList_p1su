c08497361e5bc2e49b950769a1414fcb63a6919e
P1su <pissssssu@naver.com>
2024-08-15 23:15:34 +0900

    feat: Editor 스타일 코드 적용

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -1,9 +1,11 @@
+import './Editor.css';
+
 const Editor = () => {
   return(
-    <div>
-      <form>
-        <input placeholder='새로운 Todo...' />
-        <button type='submit'>
+    <div className='editor-wrapper'>
+      <form className='form'>
+        <input className='input' placeholder='새로운 Todo...' />
+        <button className='button' type='submit'>
           추가
         </button>
       </form>
