c109dec5e50ce9948403b2f9de733a3bf465c30c
P1su <pissssssu@naver.com>
2024-08-16 00:10:08 +0900

    feat: 새로운 Todo 를 입력하여 배열에 담길 수 있도록 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
+import { useState } from 'react';
 import './App.css';
 import Editor from './components/Editor/Editor';
 import Header from './components/Header/Header';
 import List from './components/List/List';
 
+export interface Todo{
+  todo: string;
+  date: string;
+}
 function App() {
+
+  const [values, setValues] = useState<Todo[]>([]);
+
+  console.log(values);
 
   return (
     <div className='Wrapper'>
       <Header />
-      <Editor />
+      <Editor values={values} setValues={setValues}/>
       <List />
     </div>
   );
--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -1,11 +1,53 @@
 import './Editor.css';
+import type { Todo } from '../../App';
+import { useState } from 'react';
 
-const Editor = () => {
+interface EditorProps {
+  values: Todo;
+  setValues: (value: Todo) => void;
+}
+
+const Editor = ({values, setValues}: EditorProps) => {
+  const [input, setInput] = useState<string>('');
+
+  const formatDate = (): string => {
+    const date = new Date();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+
+    return `${month}월 ${day}일`;
+  } 
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  };
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setValues((prevValues: any) => [
+      ...prevValues,
+      {
+        todo: input,
+        date: formatDate(),
+      }
+    ]);
+  };
+
   return(
     <div className='editor-wrapper'>
-      <form className='form'>
-        <input className='input' placeholder='새로운 Todo...' />
-        <button className='button' type='submit'>
+      <form 
+        className='form'
+        onSubmit={handleSubmit}  
+      >
+        <input 
+          className='input' 
+          placeholder='새로운 Todo...' 
+          value={input}  
+          onChange={handleInput}
+        />
+        <button 
+          className='button' 
+          type='submit'
+        >
           추가
         </button>
       </form>
