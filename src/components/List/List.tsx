ae8300b99567098be6964d1d1c9e1734f1ebc076
P1su <pissssssu@naver.com>
2024-08-16 22:54:54 +0900

    feat: 검색기능 추가

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,20 +1,44 @@
 import './List.css';
+import { useState } from 'react';
 import TodoList from './TodoList/TodoList';
 import type {Todo} from '../../App';
 interface ListProps {
   values: Todo[];
 }
 const List = ({values}: ListProps) => {
-  console.log(values)
+  const [search, setSearch] = useState('');
+  
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const getFilteredTodo = () => {
+    console.log(search)
+    if(search === ''){
+      return values;
+    }
+    return values.filter((value) => 
+      value.todo.includes(search)
+    );
+
+  }
+
+  const filteredValues = getFilteredTodo();
+
   return(
     <div className='list-wrapper'>
       <h5 className='list-title'>Todo List</h5>
       <form>
-        <input className='search-input' placeholder='검색어를 입력하세요'/>
+        <input 
+          className='search-input' 
+          placeholder='검색어를 입력하세요'
+          value={search}
+          onChange={handleSearch}
+        />
       </form>
       <>
         {
-          values.map((value) => (
+          filteredValues.map((value) => (
             <TodoList 
               key={value.id}
               value={value}
